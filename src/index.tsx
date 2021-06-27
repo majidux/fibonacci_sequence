01f5844279d4038d1a36f8747a29c48048b0a5e2
Majid <majiddarvishnejad93@gmail.com>
2021-06-27 15:43:56 +0430

    fix: add memoization principle for improve performance

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,21 @@
-function fibonacciNumber(num: number): number {
+type FibonacciNumberTypes = {
+  num: number;
+  memo?: any;
+};
+
+function fibonacciNumber({ num, memo = {} }: FibonacciNumberTypes): number {
+  if (num in memo) return memo[num];
   if (num <= 2) return 1;
-  return fibonacciNumber(num - 1) + fibonacciNumber(num - 2);
+  memo[num] =
+    fibonacciNumber({ num: num - 1, memo }) +
+    fibonacciNumber({ num: num - 2, memo });
+  return memo[num];
 }
 
 export function fibonacciSequence(length: number = 5): number[] {
   let arr: number[] = [];
   for (let iterator: number = 1; iterator < length; iterator++) {
-    arr.push(fibonacciNumber(iterator));
+    arr.push(fibonacciNumber({ num: iterator }));
   }
   return arr;
 }
